747cd1c4e865efda6a222b52f07a74c39bf509e2
Letícia França <leeffrance@gmail.com>
2025-03-13 14:46:37 -0300

    exercises 11 - part 1

--- js/ex11todolist.js
+++ js/ex11todolist.js
@@ -6,8 +6,17 @@ function renderTodoList() {
     let todoListHTML = '';
 
     for (let i = 0; i < todoList.length; i++) {
-        const todo = todoList[i];
-        const html = `<p>${todo}</p>`;
+        const todoObject = todoList[i];
+        const { name, date } = todoObject;
+        const html = `
+        <p>
+            ${name} ${date} 
+            <button onclick="
+                todoList.splice(${i}, 1);
+                renderTodoList();
+            " class="delete-todo-button">Delete</button>
+        </p>
+        `;
         todoListHTML += html;
     }
 
@@ -16,11 +25,15 @@ function renderTodoList() {
 }
 
 function addTodo() {
-    const inputElement = document.querySelector('.name-input');
-    const name = inputElement.value;
+    const inputElementName = document.querySelector('.name-input');
+    const inputElementDate = document.querySelector('.date-input');
+    const name = inputElementName.value;
+    const date = inputElementDate.value;
     
-    todoList.push(name);
-    inputElement.value = '';
+    todoList.push({name: name, date: date});
+
+    inputElementName.value = '';
+    inputElementDate.value = '';
 
     renderTodoList();
 }
